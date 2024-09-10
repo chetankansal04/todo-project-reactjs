3fe6cddaa0369aaffdf336f90abdace99c50c78f
chetankansal04 <ckansal0@gmail.com>
2024-09-11 01:38:20 +0530

    update to-do app

--- vitereact-todo-v2/src/Components/Appname.jsx
+++
@@ -1,7 +0,0 @@
-import  css from "./Appname.module.css"
-function AppName() {
-  return <h1 className={css.todoHeading}>
-    TODO App</h1>
-}
-
-export default AppName;
--- vitereact-todo/src/Components/Appname.jsx
+++
@@ -1,5 +0,0 @@
-function AppName() {
-  return <h1>TODO App</h1>;
-}
-
-export default AppName;
